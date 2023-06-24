aad61ae3823c0b8963dc83bb487c89cdaf38f46c
1033690378 <62001246+1033690378@users.noreply.github.com>
2023-06-24 15:46:10 +0800

    Add files via upload

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -74,6 +74,44 @@ import { useStore } from 'vuex'
         setup() {
             const { proxy } = getCurrentInstance();
             let tableData = ref([]);
+            // let countData = [
+            //     {
+            //         name: "今日支付订单",
+            //         value: 1234,
+            //         icon: "SuccessFilled",
+            //         color: "#2ec7c9",
+            //     },
+            //     {
+            //         name: "今日收藏订单",
+            //         value: 210,
+            //         icon: "StarFilled",
+            //         color: "#ffb980",
+            //     },
+            //     {
+            //         name: "今日未支付订单",
+            //         value: 1234,
+            //         icon: "GoodsFilled",
+            //         color: "#5ab1ef",
+            //     },
+            //     {
+            //         name: "本月支付订单",
+            //         value: 1234,
+            //         icon: "SuccessFilled",
+            //         color: "#2ec7c9",
+            //     },
+            //     {
+            //         name: "本月收藏订单",
+            //         value: 210,
+            //         icon: "StarFilled",
+            //         color: "#ffb980",
+            //     },
+            //     {
+            //         name: "本月未支付订单",
+            //         value: 1234,
+            //         icon: "GoodsFilled",
+            //         color: "#5ab1ef",
+            //     },
+            // ]
             let countData = ref([]);
             const store = useStore()
             //获取用户权限属性
